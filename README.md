afbd8c1c2da69fc33c99f46cfe626c111e492dab
Shawn Evans <shawnevans328@gmail.com>
2025-12-17 18:02:16 -0500

    getting vars set up.... this is hard!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a Tic Tac Toe computer written in Rust.
+This is a Tic Tac Toe computer written in Rust. I am going to be using elaborate comments as I develop it, so that I may learn
+the intricacies of a rust application.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+
+    const BOARD_SIZE: usize = 9; // rust constants want this fancy decleration where we need to type hint.... ok!
+    const EMPTY: char = ' ';
+    const CROSS: char = 'X';
+    const CIRCLE: char = 'O';
+
+    let mut board: [char; BOARD_SIZE] = [EMPTY; BOARD_SIZE];
+
+    board[0] = CROSS;
+    board[3] = CIRCLE;
+
+    print_board(board);
+
+    println!("{}", board.len());
+
+}
+
+fn print_board(board: [char; 9]) {
+
+    for x in board.into_iter() {
+        print!("{} ", x);
+    }
+
 }
