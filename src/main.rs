5bdf997a6282e646284bb581e558c6e8b94fb77e
Shawn Evans <shawnevans328@gmail.com>
2025-12-18 11:21:41 -0500

    board as a matrix

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
+use std::io;
+
 fn main() {
 
-    const BOARD_SIZE: usize = 9; // rust constants want this fancy decleration where we need to type hint.... ok!
-    const EMPTY: char = ' ';
+    const BOARD_SIZE: usize = 3; // rust constants want this fancy decleration where we need to type hint.... ok!
+    const EMPTY: char = 'E';
     const CROSS: char = 'X';
     const CIRCLE: char = 'O';
 
-    let mut board: [char; BOARD_SIZE] = [EMPTY; BOARD_SIZE];
+    let mut board: [[char; BOARD_SIZE]; BOARD_SIZE] = [[EMPTY; BOARD_SIZE]; BOARD_SIZE];
 
-    board[0] = CROSS;
-    board[3] = CIRCLE;
+    board[0][0] = CROSS;
 
     print_board(board);
 
     println!("{}", board.len());
-
 }
 
-fn print_board(board: [char; 9]) {
+fn print_board(board: [[char; 3]; 3]) {
 
     for x in board.into_iter() {
-        print!("{} ", x);
+        print!("{} ", x[0]);
     }
 
 }
